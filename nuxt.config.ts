dac3ddba6473a96dba9c706d3c36e93e046f4135
Mauricio Bellon <mauriciobellon@gmail.com>
2024-11-11 22:38:29 -0300

    feat: atualiza configuração do Nuxt e dependências
    Adiciona o módulo Prisma ao projeto e configura opções
    experimentais no Nuxt. Atualiza scripts no package.json
    para incluir comandos de gerenciamento de banco de dados eajusta as dependências para versões mais recentes Essas
    mudançasam melhorar a integração o Prisma e
    facilitar o desenvolvimento e a manutenção do projeto.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,24 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-09",
   devtools: { enabled: false },
   modules: [
-    '@nuxt/test-utils/module'
-  ]
+    "@prisma/nuxt",
+    "@nuxt/test-utils"
+  ],
+  experimental: {
+    watcher: 'chokidar'
+  },
+  prisma: {
+    installCLI: false,
+    installClient: false,
+    generateClient: false,
+    installStudio: true,
+    autoSetupPrisma: true,
+  },
+  vite: {
+    resolve: {
+      alias: {
+        '.prisma/client/index-browser': './node_modules/.pnpm/@prisma+client@5.22.0_prisma@5.22.0/node_modules/.prisma/client/index-browser.js',
+      },
+    },
+  },
 })
