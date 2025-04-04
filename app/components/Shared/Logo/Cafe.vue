3f9f91fb119e93d9afea820f5ee9f69f24e88ef6
Ingrid Hayuni <ingrid.searom@gmail.com>
2025-04-03 21:09:26 -0300

    Atualiza logo do café e ícone de grão

--- app/components/Shared/Logo/Cafe.vue
+++ app/components/Shared/Logo/Cafe.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-black px-2 py-2 -rotate-3">
+  <div class="bg-[#008000] px-2 py-2 -rotate-3">
     <div class="text-white text-[24px] subpixel-antialiased rotate-3">
       café
     </div>
